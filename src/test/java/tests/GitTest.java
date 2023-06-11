bd340739530330926dd3fb8f943f37590e863194
Alexander <WebDer11@gmail.com>
2023-06-11 23:22:12 +0400

    Final Test

--- src/test/java/tests/GitTest.java
+++ src/test/java/tests/GitTest.java
@@ -1,4 +1,40 @@
 package tests;
 
-public class GitTest {
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitTest extends TestBase{
+
+
+    @Test
+    void gitSoftAssertiions() {
+
+
+
+        open("/selenide/selenide");
+
+        $("#wiki-tab").click();
+
+        $("#wiki-pages-box").$("[type='button']").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("[data-filterable-for=wiki-pages-filter]").shouldHave(text("SoftAssertions"));
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $("#wiki-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                """));
+
+
+    }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,18 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+
+
 public class TestBase {
+
+    @BeforeAll
     static void beforeAll() {
+        Configuration.baseUrl ="https://github.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
-        Configuration.baseUrl = "https://demoqa.com";
         Configuration.savePageSource = false;
         Configuration.screenshots = false;
     }
-}
 }
